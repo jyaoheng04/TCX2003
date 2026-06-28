e8f65c894f392a21b28f9b6ba34b05338e22adc7
Jie Yao <jieyaoheng04@gmail.com>
2026-06-28 15:16:24 +0800

    show queue number on patient frontend and make register.html scrollable.

--- routes/patient.py
+++ routes/patient.py
@@ -26,31 +26,31 @@ def get_db():
 # ======================
 # GET LOGGED IN PATIENT ID
 # ======================
-# def get_logged_in_patient_id():
+def get_logged_in_patient_id():
 
-#     user_id = session.get("user_id")
+    user_id = session.get("user_id")
 
-#     if not user_id:
-#         return None
+    if not user_id:
+        return None
 
-#     db = get_db()
-#     cursor = db.cursor(dictionary=True)
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
 
-#     cursor.execute("""
-#         SELECT patient_id
-#         FROM patient
-#         WHERE user_id = %s
-#     """, (user_id,))
+    cursor.execute("""
+        SELECT patient_id
+        FROM patient
+        WHERE user_id = %s
+    """, (user_id,))
 
-#     patient = cursor.fetchone()
+    patient = cursor.fetchone()
 
-#     cursor.close()
-#     db.close()
+    cursor.close()
+    db.close()
 
-#     if patient:
-#         return patient["patient_id"]
+    if patient:
+        return patient["patient_id"]
 
-#     return None
+    return None
 
 # ======================
 # VIEW APPOINTMENTS
@@ -62,12 +62,12 @@ def appointments():
     cursor = db.cursor(dictionary=True)
 
     # temporary hardcoded patient
-    patient_id = 1
+    # patient_id = 1
 
-    # patient_id = get_logged_in_patient_id()
+    patient_id = get_logged_in_patient_id()
 
-    # if not patient_id:
-    #     return redirect("/")
+    if not patient_id:
+        return redirect("/")
 
     # auto-complete appointments whose datetime has passed
     cursor.execute("""
@@ -87,10 +87,13 @@ def appointments():
             a.reason,
             a.appointment_date,
             a.queue_status,
+            q.queue_number,
             ms.full_name AS doctor_name
         FROM appointment a
         LEFT JOIN medical_staff ms
             ON a.doctor_id = ms.staff_id
+        LEFT JOIN queue q
+            ON a.appointment_id = q.appointment_id
         WHERE a.patient_id = %s
         ORDER BY a.appointment_date ASC
     """, (patient_id,))
@@ -205,12 +208,12 @@ def create():
         # ======================
         # INSERT APPOINTMENT
         # ======================
-        patient_id = 1
+        # patient_id = 1
 
-        # patient_id = get_logged_in_patient_id()
+        patient_id = get_logged_in_patient_id()
 
-        # if not patient_id:
-        #     return redirect("/")
+        if not patient_id:
+            return redirect("/")
 
         cursor.execute("""
             INSERT INTO appointment
@@ -389,12 +392,12 @@ def edit_appointment(appointment_id):
     db = get_db()
     cursor = db.cursor(dictionary=True)
 
-    patient_id = 1
+    # patient_id = 1
 
-    # patient_id = get_logged_in_patient_id()
+    patient_id = get_logged_in_patient_id()
 
-    # if not patient_id:
-    #     return redirect("/")
+    if not patient_id:
+        return redirect("/")
 
     # verify ownership
     cursor.execute("""
@@ -549,12 +552,12 @@ def cancel_appointment(appointment_id):
     db = get_db()
     cursor = db.cursor(dictionary=True)
 
-    patient_id = 1
+    # patient_id = 1
 
-    # patient_id = get_logged_in_patient_id()
+    patient_id = get_logged_in_patient_id()
 
-    # if not patient_id:
-    #     return redirect("/")
+    if not patient_id:
+        return redirect("/")
 
     # verify ownership
     cursor.execute("""
@@ -645,12 +648,12 @@ def profile():
     db = get_db()
     cursor = db.cursor(dictionary=True)
 
-    patient_id = 1  
+    # patient_id = 1  
 
-    # patient_id = get_logged_in_patient_id()
+    patient_id = get_logged_in_patient_id()
 
-    # if not patient_id:
-    #     return redirect("/")
+    if not patient_id:
+        return redirect("/")
 
     cursor.execute("""
         SELECT
@@ -686,12 +689,12 @@ def records():
     db = get_db()
     cursor = db.cursor(dictionary=True)
 
-    patient_id = 1
+    # patient_id = 1
 
-    # patient_id = get_logged_in_patient_id()
+    patient_id = get_logged_in_patient_id()
 
-    # if not patient_id:
-    #     return redirect("/")
+    if not patient_id:
+        return redirect("/")
 
     cursor.execute("""
         SELECT
@@ -773,12 +776,12 @@ def view_report(consultation_id):
     db = get_db()
     cursor = db.cursor(dictionary=True)
 
-    patient_id = 1
+    # patient_id = 1
 
-    # patient_id = get_logged_in_patient_id()
+    patient_id = get_logged_in_patient_id()
 
-    # if not patient_id:
-    #     return redirect("/")
+    if not patient_id:
+        return redirect("/")
 
     # main consultation
     cursor.execute("""
@@ -980,12 +983,12 @@ def dashboard():
 
     cursor = conn.cursor(dictionary=True)
 
-    patient_id = 1
+    # patient_id = 1
 
-    # patient_id = get_logged_in_patient_id()
+    patient_id = get_logged_in_patient_id()
 
-    # if not patient_id:
-    #     return redirect("/")
+    if not patient_id:
+        return redirect("/")
 
     today = date.today()
 
@@ -993,11 +996,14 @@ def dashboard():
     cursor.execute("""
         SELECT
             a.*,
+            q.queue_number,
             m.full_name AS doctor_name,
             m.department
         FROM appointment a
         LEFT JOIN medical_staff m
             ON a.doctor_id = m.staff_id
+        LEFT JOIN queue q
+            ON a.appointment_id = q.appointment_id
         WHERE a.patient_id = %s
         AND DATE(a.appointment_date) = %s
         AND a.queue_status != 'cancelled'
@@ -1010,11 +1016,14 @@ def dashboard():
     cursor.execute("""
         SELECT
             a.*,
+            q.queue_number,
             m.full_name AS doctor_name,
             m.department
         FROM appointment a
         LEFT JOIN medical_staff m
             ON a.doctor_id = m.staff_id
+        LEFT JOIN queue q
+            ON a.appointment_id = q.appointment_id
         WHERE a.patient_id = %s
         AND DATE(a.appointment_date) > %s
         AND a.queue_status != 'cancelled'
@@ -1119,12 +1128,12 @@ def create_multi():
 
     if request.method == 'POST':
 
-        patient_id = 1
+        # patient_id = 1
 
-        # patient_id = get_logged_in_patient_id()
+        patient_id = get_logged_in_patient_id()
 
-        # if not patient_id:
-        #     return redirect("/")
+        if not patient_id:
+            return redirect("/")
 
         errors = {}
 
@@ -1246,13 +1255,94 @@ def create_multi():
 
             appointment_id = cursor.lastrowid
 
+            # ======================
+            # GENERATE QUEUE NUMBER
+            # ======================
+
+            prefix = "C" if t == "consultation" else "L"
+
+            cursor.execute("""
+                SELECT queue_number
+                FROM queue
+                WHERE queue_number LIKE %s
+                ORDER BY queue_id DESC
+                LIMIT 1
+            """, (prefix + "%",))
+
+            last = cursor.fetchone()
+
+            if last is None:
+                next_number = 1
+            else:
+                last_num = int(last["queue_number"][1:])
+                next_number = last_num + 1
+
+            queue_number = f"{prefix}{next_number:03d}"
+
+            # ======================
+            # GET ROOM ID
+            # ======================
+
+            if t == "consultation":
+
+                cursor.execute("""
+                    SELECT room_id
+                    FROM medical_staff
+                    WHERE staff_id = %s
+                """, (doctor_id,))
+
+                room_id = cursor.fetchone()["room_id"]
+
+            else:
+
+                cursor.execute("""
+                    SELECT room_id
+                    FROM medical_room
+                    WHERE room_type = 'laboratory'
+                    ORDER BY RAND()
+                    LIMIT 1
+                """)
+
+                room_id = cursor.fetchone()["room_id"]
+
+
+            # ======================
+            # INSERT QUEUE
+            # ======================
+
+            cursor.execute("""
+                INSERT INTO queue (
+                    appointment_id,
+                    patient_id,
+                    assigned_staff_id,
+                    room_id,
+                    queue_number,
+                    queue_status
+                )
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """, (
+                appointment_id,
+                patient_id,
+                doctor_id if t == "consultation" else None,
+                room_id,
+                queue_number,
+                "waiting"
+            ))
+
+            queue_id = cursor.lastrowid
+
             # link appointment to consultation
             cursor.execute("""
                 UPDATE consultation
-                SET appointment_id = %s
+                SET appointment_id = %s,
+                    queue_id = %s
                 WHERE consultation_id = %s
                 AND appointment_id IS NULL
-            """, (appointment_id, consultation_id))
+            """, (
+                appointment_id,
+                queue_id,
+                consultation_id
+            ))
 
             # insert lab result for lab types
             if t in ('blood_test', 'urine_test'):
